b433d551bdbf2597de90fa6416783a352bf55c08
EleahBurman <eleahjburman@gmail.com>
2023-05-19 09:44:13 -0400

    Class for name and boards and transition for hover

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -9,31 +9,31 @@ import styles from './ProfileCard.module.css'
 
 const ProfileCard = ({ profile }) => {
   const photo = profile.photo ? profile.photo : tacoman
-  const cardStyle = {
-    width: '400px',
-    height: '400px',
-  }
   if (profile.image) {
     return (
       <Link to={`/profiles/${profile._id}`}>
-        <main className={`${styles.profilecardcontainer}`} style={cardStyle}>
+        <main className={`${styles.profilecardcontainer}`}>
           <div className={`${styles.profileimage}`}>
-          <img style={{width: '200px', height: '200px'}} src={photo} alt="Profile Photo" />
+            <img style={{width: '200px', height: '200px'}} src={photo} alt="Profile Photo" />
           </div>
-          <h3>{profile.name}</h3>
-          <h4>{profile.boards.length} Boards</h4>
+          <div className={`${styles.nameandboards}`}>
+            <h3>{profile.name}</h3>
+            <h4>{profile.boards.length} Boards</h4>
+          </div>
         </main>
       </Link>
     )
   } else {
     return (
       <Link to={`/profiles/${profile._id}`} >
-        <main className={`${styles.profilecardcontainer}`} style={cardStyle}>
+        <main className={`${styles.profilecardcontainer}`}>
           <div className={`${styles.noprofileimage}`}>
-          <img style={{width: '200px', height: '200px'}} src={photo} alt="Taco Man" />
+            <img style={{width: '200px', height: '200px'}} src={photo} alt="Taco Man" />
           </div>
-          <h3>{profile.name}</h3>
-          <h4>{profile.boards.length} Boards</h4>
+          <div className={`${styles.nameandboards}`}>
+            <h3>{profile.name}</h3>
+            <h4>{profile.boards.length} Boards</h4>
+          </div>
         </main>
       </Link>
     )
